365cdfdb842f6a9bb8ff4dfd61d5fefb2bbee630
alberto bisello <alberto.bisello@warda.it>
2018-03-25 16:06:42 +0200

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list_item'
-import youtubeSearch from 'youtube-api-search'
 
 const API_KEY_YOUTUBE = 'AIzaSyAgFZc8KejemmzoR6qzunRH4OyoYWXg34Y'
-
-youtubeSearch(API_KEY_YOUTUBE,{
-  q:'nodejs',
-  part:'snippet',
-  type:'video'
-}, (videos)=> {
-  console.log(videos)
-})
 
 const App = () => {
   return <div>
